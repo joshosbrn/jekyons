c49a1c2f26b6b35510e700a0c0f358744d9bdd89
Josh Osborne <joshosbrn@gmail.com>
2016-08-15 01:13:12 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+var browserSync = require('browser-sync').create();
+
+// Task for building blog when something changed:
+gulp.task('build', shell.task(['bundle exec jekyll build --watch']));
+// Or if you don't use bundle:
+// gulp.task('build', shell.task(['jekyll build --watch']));
+
+// Task for serving blog with Browsersync
+gulp.task('serve', function () {
+    browserSync.init({server: {baseDir: '_site/'}});
+    // Reloads page when some of the already built files changed:
+    gulp.watch('_site/**/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['build', 'serve']);
